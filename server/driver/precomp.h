cf5f68e980d7e961b6cf7e3a69025ce7a332d0dd
lcxl <lcx87654321@163.com>
2014-03-13 13:10:33 +0800

    移除Server端代码，Loader端和Server端将使用相同的代码。

--- server/driver/precomp.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PRE_COMP_H_
-#define _PRE_COMP_H_
-#pragma warning(disable:4201)  //nonstandard extension used : nameless struct/union
-#include <ndis.h>
-#include <filteruser.h>
-#include "flt_dbg.h"
-#include "filter.h"
-#endif
